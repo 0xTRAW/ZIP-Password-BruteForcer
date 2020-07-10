b71b8d52cf177fb9493d1ebf6baf5c313949642b
Specter Traww <61482810+specter-traww@users.noreply.github.com>
2020-07-10 16:54:26 +0000

    Update ZIP-Password-BruteForcer.py
    Making some miner changes and removing  repeating references/importies

--- ZIP-Password-BruteForcer.py
+++ ZIP-Password-BruteForcer.py
@@ -1,35 +1,38 @@
+#!/usr/bin/python
 import os
+import zipfile
+from time import time
+
+
 def cls():
 	linux = 'clear'
 	windows = 'cls'
 	os.system([linux, windows][os.name == 'nt'])
-cls()
-banner = '\n ###################################\n'
-banner += ' # ZIP Password BruteForcer        #\n'
-banner += ' ###################################\n'
-banner += ' # Coded By Sir.4m1R               #\n'
-banner += ' # The404Hacking                   #\n'
-banner += ' # Digital Security ReSearch Group #\n'
-banner += ' # T.me/The404Hacking              #\n'
-banner += ' ###################################\n'
-banner += ' GitHub:\n'
-banner += ' https://github.com/The404Hacking/ZIP-Password-BruteForcer\n\n'
-banner += ' [1] Zip Password Cracker\n'
-banner += ' [0] Exit\n'
-print banner
+
+def banner_display():
+    cls()
+    banner = '\n ###################################\n'
+    banner += ' # ZIP Password BruteForcer        #\n'
+    banner += ' ###################################\n'
+    banner += ' # Coded By Sir.4m1R               #\n'
+    banner += ' # The404Hacking                   #\n'
+    banner += ' # Digital Security ReSearch Group #\n'
+    banner += ' # T.me/The404Hacking              #\n'
+    banner += ' ###################################\n'
+    banner += ' GitHub:\n'
+    banner += ' https://github.com/The404Hacking/ZIP-Password-BruteForcer\n\n'
+    banner += ' [1] Zip Password Cracker\n'
+    banner += ' [0] Exit\n'
+
+banner_display()
 
 a=input(" [?] Enter Number : ")
 if a==0:
- import os
  cls()
  print " [!] Good Bye :)"
  quit()
 elif a==1:
- #!/usr/bin/python
 
- import zipfile
- import os
- from time import time
  
  cls()
  textzippass = '''
@@ -39,12 +42,15 @@ elif a==1:
  # The404Hacking                         #
  # Digital Security ReSearch Group       #
  # T.me/The404Hacking                    #
+ # Edited by spectertraww                #
+ # https://t.me/spectertraww             #
  #########################################
  '''
  print textzippass
- file_path = raw_input (" [+] ZIP File Address: ")
+ 
+ file_path = raw_input (" [+] ZIP File  name/path: ")
  print ""
- word_list = raw_input (" [+] Password List Address: ")
+ word_list = raw_input (" [+] Password List filename/path: ")
  def main(file_path, word_list):
 	try:
 		zip_ = zipfile.ZipFile(file_path)
@@ -70,4 +76,5 @@ elif a==1:
 				pass
 		print " [X] Sorry, Password Not Found :("
 		quit()
- main(file_path, word_list)
+if __name__ = "__main__":
+     main(file_path, word_list)
